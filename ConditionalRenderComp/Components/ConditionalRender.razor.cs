dcf00a6a2049ef3983c7c8c5d0e7a4666ef2fc34
Kevin Valmorbida <kevinvalmo@gmail.com>
2020-12-19 14:48:00 +0100

    Add project files.

---
+++ ConditionalRenderComp/Components/ConditionalRender.razor.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+
+
+namespace ConditionalRenderComp.Components
+{
+    public partial class ConditionalRender
+    {
+        [Parameter]
+        public Func<bool> Condition { get; set; }
+
+        private bool ConditionValue
+        {
+            get
+            {
+                return Condition != null && Condition.Invoke();
+            }
+        }
+
+        [Parameter]
+        public RenderFragment TrueCondition { get; set; }
+        [Parameter]
+        public RenderFragment FalseCondition { get; set; }
+
+        public ConditionalRender()
+        {
+        }
+    }
+}
---
+++ ConditionalRenderComp/Pages/Index.razor.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConditionalRenderComp.Pages
+{
+    public partial class Index
+    {
+        private bool myBool;
+        private async Task OnClick(MouseEventArgs args)
+        {
+            await InvokeAsync(
+                () => {
+                    myBool = !myBool;
+                });
+        }
+    }
+}
